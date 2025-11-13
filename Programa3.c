1fae93b887df6544af912cb9a48d39c12460191e
Amad3uXs <davidmartveg@gmail.com>
2025-11-13 13:52:32 -0600

    Create Programa3.c

---
+++ Programa3.c
@@ -0,0 +1,16 @@
+/*David Alejandro Martínez Vega L´Englisse
+practica 11 ejercicio 2a*/
+
+#include <stdio.h>
+int resultado; //variable global
+void multiplicar(); // prototipo de la función
+int main()
+{
+ multiplicar(); //llamado de la función multiplicar
+ printf("%i",resultado);
+ return 0;
+}
+void multiplicar() //función multiplicar
+{
+ resultado = 5 * 4;
+}
